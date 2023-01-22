521ba232feb9221601c01599d5c0462b4ee3708b
beskay <beskay0x@gmail.com>
2023-01-22 14:47:07 +0100

    Refactor Orders.sol, fix stack too deep

--- contracts/api/Orders.sol
+++ contracts/api/Orders.sol
@@ -87,6 +87,7 @@ contract Orders is Roles {
 
         (, vc1) = _submitOrder(params);
 
+        // tp/sl price checks
         if (tpPrice > 0 || slPrice > 0) {
             if (params.price > 0) {
                 if (tpPrice > 0) {
@@ -107,13 +108,11 @@ contract Orders is Roles {
                 require((params.isLong && tpPrice > slPrice) || (!params.isLong && tpPrice < slPrice), '!tpsl-invalid');
             }
 
+            // submit TP/SL orders
             uint32 tpOrderId;
             uint32 slOrderId;
 
-            // todo: remove this
-            if (tpPrice > 0 || slPrice > 0) {
-                params.isLong = !params.isLong;
-            }
+            params.isLong = !params.isLong;
 
             if (tpPrice > 0) {
                 params.price = tpPrice;
@@ -145,96 +144,94 @@ contract Orders is Roles {
     }
 
     function _submitOrder(OrderStore.Order memory params) internal returns (uint32, uint256) {
-        address user = msg.sender;
-
         // console.log(1);
 
         // Validations
+        {
+            require(params.orderType == 0 || params.orderType == 1 || params.orderType == 2, '!order-type');
 
-        require(params.orderType == 0 || params.orderType == 1 || params.orderType == 2, '!order-type');
-
-        if (params.orderType != 0) {
-            require(params.price > 0, '!price');
-        }
-
-        // console.log(2);
-
-        AssetStore.Asset memory asset = assetStore.get(params.asset);
-        require(asset.minSize > 0, '!asset-exists');
-        require(params.size >= asset.minSize, '!min-size');
-
-        // console.log(3);
-
-        MarketStore.Market memory market = marketStore.get(params.market);
-        require(market.maxLeverage > 0, '!market-exists');
-
-        // console.log(5);
-
-        if (params.expiry > 0) {
-            require(params.expiry >= block.timestamp, '!expiry-value');
-            uint256 ttl = params.expiry - block.timestamp;
-            require(
-                (params.orderType == 0 && ttl <= orderStore.maxMarketOrderTTL()) ||
-                    ttl <= orderStore.maxTriggerOrderTTL(),
-                '!max-expiry'
-            );
-        }
-
-        // console.log(6);
-
-        if (params.cancelOrderId > 0) {
-            require(orderStore.isUserOrder(params.cancelOrderId, user), '!user-oco');
-        }
-
-        // console.log(7);
-
-        uint256 fee = (params.size * market.fee) / BPS_DIVIDER;
-        uint256 valueConsumed;
-
-        if (params.isReduceOnly) {
-            params.margin = 0;
-            // Existing position is checked on execution so TP/SL can be submitted as reduce-only alongside a non-executed order
-            // In this case, valueConsumed is zero as margin is zero and fee is taken from the order's margin
-        } else {
-            require(!market.isReduceOnly, '!market-reduce-only');
-            require(params.margin > 0, '!margin');
-
-            uint256 leverage = (UNIT * params.size) / params.margin;
-            require(leverage >= UNIT, '!min-leverage');
-            require(leverage <= market.maxLeverage * UNIT, '!max-leverage');
-
-            // console.log(71);
-            // Check against max OI if it's not reduce-only. this is not completely fail safe as user can place many consecutive market orders of smaller size and get past the max OI limit here, because OI is not updated until keeper picks up the order. That is why maxOI is checked on processing as well, which is fail safe. This check is more of preemptive for user to not submit an order
-            riskStore.checkMaxOI(params.asset, params.market, params.size);
-            // console.log(72);
-
-            // Transfer fee and margin to store
-            valueConsumed = params.margin + fee;
-
-            if (params.asset == address(0)) {
-                fundStore.transferIn{value: valueConsumed}(params.asset, user, valueConsumed);
-            } else {
-                fundStore.transferIn(params.asset, user, valueConsumed);
+            if (params.orderType != 0) {
+                require(params.price > 0, '!price');
             }
 
-            // console.log(73);
+            // console.log(2);
+
+            AssetStore.Asset memory asset = assetStore.get(params.asset);
+            require(asset.minSize > 0, '!asset-exists');
+            require(params.size >= asset.minSize, '!min-size');
+
+            // console.log(3);
+
+            MarketStore.Market memory market = marketStore.get(params.market);
+            require(market.maxLeverage > 0, '!market-exists');
+
+            // console.log(5);
+
+            if (params.expiry > 0) {
+                require(params.expiry >= block.timestamp, '!expiry-value');
+                uint256 ttl = params.expiry - block.timestamp;
+                require(
+                    (params.orderType == 0 && ttl <= orderStore.maxMarketOrderTTL()) ||
+                        ttl <= orderStore.maxTriggerOrderTTL(),
+                    '!max-expiry'
+                );
+            }
+
+            // console.log(6);
+
+            if (params.cancelOrderId > 0) {
+                require(orderStore.isUserOrder(params.cancelOrderId, msg.sender), '!user-oco');
+            }
+
+            if (!params.isReduceOnly) {
+                require(!market.isReduceOnly, '!market-reduce-only');
+                require(params.margin > 0, '!margin');
+
+                uint256 leverage = (UNIT * params.size) / params.margin;
+                require(leverage >= UNIT, '!min-leverage');
+                require(leverage <= market.maxLeverage * UNIT, '!max-leverage');
+            }
+        }
+        // console.log(7);
+
+        // Set order params
+        {
+            params.user = msg.sender;
+            params.timestamp = uint32(block.timestamp);
+
+            MarketStore.Market memory market = marketStore.get(params.market);
+            uint256 fee = (params.size * market.fee) / BPS_DIVIDER;
+            params.fee = fee;
+        }
+
+        if (params.isReduceOnly) {
+            // Existing position is checked on execution so TP/SL can be submitted as reduce-only alongside a non-executed order
+            // In this case, valueConsumed is zero as margin is zero and fee is taken from the order's margin
+            params.margin = 0;
+        } else {
+            // Check against max OI if it's not reduce-only. this is not completely fail safe as user can place many consecutive market orders of smaller size and get past the max OI limit here, because OI is not updated until keeper picks up the order. That is why maxOI is checked on processing as well, which is fail safe. This check is more of preemptive for user to not submit an order
+            riskStore.checkMaxOI(params.asset, params.market, params.size);
+
+            // Transfer fee and margin to store
+            uint256 valueConsumed = params.margin + params.fee;
+
+            if (params.asset == address(0)) {
+                fundStore.transferIn{value: valueConsumed}(params.asset, params.user, valueConsumed);
+            } else {
+                fundStore.transferIn(params.asset, params.user, valueConsumed);
+            }
         }
 
         // console.log(8);
 
         // Add order to store
-
-        params.user = user;
-        params.fee = fee;
-        params.timestamp = uint32(block.timestamp);
-
-        uint32 orderId = orderStore.add(params);
+        params.orderId = orderStore.add(params);
 
         // console.log(9);
 
         emit OrderCreated(
-            orderId,
-            user,
+            params.orderId,
+            params.user,
             params.asset,
             params.market,
             params.isLong,
@@ -248,7 +245,7 @@ contract Orders is Roles {
             params.cancelOrderId
         );
 
-        return (orderId, valueConsumed);
+        return (params.orderId, params.isReduceOnly ? 0 : params.margin + params.fee);
     }
 
     function cancelOrder(uint32 orderId) external ifNotPaused {
