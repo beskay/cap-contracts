acd6dccedb5299aea879c8f627f8aa5693e44824
beskay <beskay0x@gmail.com>
2023-01-31 23:27:59 +0100

    Add comments, remove unused isGlobalReduceOnly

--- contracts/stores/MarketStore.sol
+++ contracts/stores/MarketStore.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.13;
 
 import '../utils/Roles.sol';
 
+/// @title MarketStore
+/// @notice Persistent storage of supported markets
 contract MarketStore is Roles {
+    // Market struct
     struct Market {
         string name; // Bitcoin / U.S. Dollar
         string category; // crypto, fx, commodities, indices
@@ -20,26 +23,23 @@ contract MarketStore is Roles {
         bool isReduceOnly; // accepts only reduce only orders
     }
 
+    // Constants to limit gov power
     uint256 public constant MAX_FEE = 1000; // 10%
     uint256 public constant MAX_DEVIATION = 1000; // 10%
     uint256 public constant MAX_LIQTHRESHOLD = 10000; // 100%
     uint256 public constant MAX_MIN_ORDER_AGE = 30;
     uint256 public constant MIN_PYTH_MAX_AGE = 3;
 
+    // list of supported markets
     string[] public marketList; // "ETH-USD", "BTC-USD", etc
-
     mapping(string => Market) private markets;
-
-    bool public isGlobalReduceOnly;
 
     constructor(RoleStore rs) Roles(rs) {}
 
-    // Setters
-
-    function setIsGlobalReduceOnly(bool b) external onlyGov {
-        isGlobalReduceOnly = b;
-    }
-
+    /// @notice Set or update a market
+    /// @dev Only callable by governance
+    /// @param market String identifier, e.g. "ETH-USD"
+    /// @param marketInfo Market struct containing required market data
     function set(string calldata market, Market memory marketInfo) external onlyGov {
         require(marketInfo.fee <= MAX_FEE, '!max-fee');
         require(marketInfo.maxLeverage >= 1, '!max-leverage');
@@ -50,17 +50,20 @@ contract MarketStore is Roles {
 
         markets[market] = marketInfo;
         for (uint256 i = 0; i < marketList.length; i++) {
+            // check if market already exists, if yes return
             if (keccak256(abi.encodePacked(marketList[i])) == keccak256(abi.encodePacked(market))) return;
         }
         marketList.push(market);
     }
 
-    // Getters
-
+    /// @notice Returns market struct of `market`
+    /// @param market String identifier, e.g. "ETH-USD"
     function get(string calldata market) external view returns (Market memory) {
         return markets[market];
     }
 
+    /// @notice Returns market struct array of specified markets
+    /// @param _markets Array of market strings, e.g. ["ETH-USD", "BTC-USD"]
     function getMany(string[] calldata _markets) external view returns (Market[] memory _marketInfos) {
         uint256 length = _markets.length;
         _marketInfos = new Market[](length);
@@ -70,14 +73,18 @@ contract MarketStore is Roles {
         return _marketInfos;
     }
 
+    /// @notice Returns market identifier at `index`
+    /// @param index index of marketList
     function getMarketByIndex(uint256 index) external view returns (string memory) {
         return marketList[index];
     }
 
+    /// @notice Get a list of all supported markets
     function getMarketList() external view returns (string[] memory) {
         return marketList;
     }
 
+    /// @notice Get number of supported markets
     function getMarketCount() external view returns (uint256) {
         return marketList.length;
     }
