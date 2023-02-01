4297ccb5df7c9b4e0d18ef41624816d8f9265326
beskay <beskay0x@gmail.com>
2023-02-01 17:32:14 +0100

    Add comments, remove return var name

--- contracts/stores/OrderStore.sol
+++ contracts/stores/OrderStore.sol
@@ -5,9 +5,13 @@ import '@openzeppelin/contracts/utils/structs/EnumerableSet.sol';
 
 import '../utils/Roles.sol';
 
+/// @title OrderStore
+/// @notice Persistent storage for Orders.sol
 contract OrderStore is Roles {
+    // Libraries
     using EnumerableSet for EnumerableSet.UintSet;
 
+    // Order struct
     struct Order {
         uint256 orderId;
         address user;
@@ -31,36 +35,52 @@ contract OrderStore is Roles {
     EnumerableSet.UintSet private marketOrderIds; // [order ids..]
     EnumerableSet.UintSet private triggerOrderIds; // [order ids..]
 
-    bool public areNewOrdersPaused;
-    bool public isProcessingPaused;
     uint256 public maxMarketOrderTTL = 5 minutes;
     uint256 public maxTriggerOrderTTL = 180 days;
     uint256 public chainlinkCooldown = 5 minutes;
+
+    bool public areNewOrdersPaused;
+    bool public isProcessingPaused;
 
     constructor(RoleStore rs) Roles(rs) {}
 
     // Setters
 
+    /// @notice Disable submitting new orders
+    /// @dev Only callable by governance
     function setAreNewOrdersPaused(bool b) external onlyGov {
         areNewOrdersPaused = b;
     }
 
+    /// @notice Disable processing new orders
+    /// @dev Only callable by governance
     function setIsProcessingPaused(bool b) external onlyGov {
         isProcessingPaused = b;
     }
 
+    /// @notice Set duration until market orders expire
+    /// @dev Only callable by governance
+    /// @param amount Duration in seconds
     function setMaxMarketOrderTTL(uint256 amount) external onlyGov {
         maxMarketOrderTTL = amount;
     }
 
+    /// @notice Set duration until trigger orders expire
+    /// @dev Only callable by governance
+    /// @param amount Duration in seconds
     function setMaxTriggerOrderTTL(uint256 amount) external onlyGov {
         maxTriggerOrderTTL = amount;
     }
 
+    /// @notice Set duration after orders can be executed with chainlink
+    /// @dev Only callable by governance
+    /// @param amount Duration in seconds
     function setChainlinkCooldown(uint256 amount) external onlyGov {
         chainlinkCooldown = amount;
     }
 
+    /// @notice Adds order to storage
+    /// @dev Only callable by other protocol contracts
     function add(Order memory order) external onlyContract returns (uint256) {
         uint256 nextOrderId = ++oid;
         order.orderId = nextOrderId;
@@ -74,6 +94,8 @@ contract OrderStore is Roles {
         return nextOrderId;
     }
 
+    /// @notice Removes order from store
+    /// @dev Only callable by other protocol contracts
     function remove(uint256 orderId) external onlyContract {
         Order memory order = orders[orderId];
         if (order.size == 0) return;
@@ -83,67 +105,91 @@ contract OrderStore is Roles {
         delete orders[orderId];
     }
 
+    /// @notice Updates `cancelOrderId` of `orderId`, e.g. TP order cancels a SL order and vice versa
+    /// @dev Only callable by other protocol contracts
     function updateCancelOrderId(uint256 orderId, uint256 cancelOrderId) external onlyContract {
         Order storage order = orders[orderId];
         order.cancelOrderId = cancelOrderId;
     }
 
-    // Getters
-
+    /// @notice Returns order with `orderId`
+    /// @param orderId Order to get
     function get(uint256 orderId) external view returns (Order memory) {
         return orders[orderId];
     }
 
-    function getMany(uint256[] calldata orderIds) external view returns (Order[] memory _orders) {
+    /// @notice Returns orders with `orderIds`
+    /// @param orderIds Orders to get, e.g. [1, 2, 5]
+    function getMany(uint256[] calldata orderIds) external view returns (Order[] memory) {
         uint256 length = orderIds.length;
-        _orders = new Order[](length);
+        Order[] memory _orders = new Order[](length);
+
         for (uint256 i = 0; i < length; i++) {
             _orders[i] = orders[orderIds[i]];
         }
+
         return _orders;
     }
 
-    function getMarketOrders(uint256 length) external view returns (Order[] memory _orders) {
+    /// @notice Returns market orders
+    /// @param length Amount of market orders to return
+    function getMarketOrders(uint256 length) external view returns (Order[] memory) {
         uint256 _length = marketOrderIds.length();
         if (length > _length) length = _length;
-        _orders = new Order[](length);
+
+        Order[] memory _orders = new Order[](length);
+
         for (uint256 i = 0; i < length; i++) {
             _orders[i] = orders[marketOrderIds.at(i)];
         }
+
         return _orders;
     }
 
-    function getTriggerOrders(uint256 length, uint256 offset) external view returns (Order[] memory _orders) {
+    /// @notice Returns trigger orders
+    /// @param length Amount of trigger orders to return
+    /// @param offset Offset to start
+    function getTriggerOrders(uint256 length, uint256 offset) external view returns (Order[] memory) {
         uint256 _length = triggerOrderIds.length();
         if (length > _length) length = _length;
-        _orders = new Order[](length);
+
+        Order[] memory _orders = new Order[](length);
+
         for (uint256 i = offset; i < length + offset; i++) {
             _orders[i] = orders[triggerOrderIds.at(i)];
         }
+
         return _orders;
     }
 
-    function getUserOrders(address user) external view returns (Order[] memory _orders) {
+    /// @notice Returns orders of `user`
+    function getUserOrders(address user) external view returns (Order[] memory) {
         uint256 length = userOrderIds[user].length();
-        _orders = new Order[](length);
+        Order[] memory _orders = new Order[](length);
+
         for (uint256 i = 0; i < length; i++) {
             _orders[i] = orders[userOrderIds[user].at(i)];
         }
+
         return _orders;
     }
 
+    /// @notice Returns amount of market orders
     function getMarketOrderCount() external view returns (uint256) {
         return marketOrderIds.length();
     }
 
+    /// @notice Returns amount of trigger orders
     function getTriggerOrderCount() external view returns (uint256) {
         return triggerOrderIds.length();
     }
 
+    /// @notice Returns order amount of `user`
     function getUserOrderCount(address user) external view returns (uint256) {
         return userOrderIds[user].length();
     }
 
+    /// @notice Returns true if order with `orderId` is from `user`
     function isUserOrder(uint256 orderId, address user) external view returns (bool) {
         return userOrderIds[user].contains(orderId);
     }
